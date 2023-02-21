c2eb4ef0838a3f60ab5c0cb88ac648d5c542a8f8
Alex <alexey85p@yandex.ru>
2023-02-21 13:51:38 +0800

    New_Portal D.4.7 login/logout/registration buttons

--- News_Portal/urls.py
+++ News_Portal/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("allauth.urls")),
     path('', include('news.urls')),
 ]
